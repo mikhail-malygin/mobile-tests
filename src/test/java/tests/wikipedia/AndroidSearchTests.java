df4752125d409115276dde5c9a79f83b8d4052c8
Mikhail Malygin <mihail.malygin@gmail.com>
2022-08-21 19:23:36 +0500

    added OpenArticleInWikipediaTests

---
+++ src/test/java/tests/wikipedia/AndroidSearchTests.java
@@ -0,0 +1,28 @@
+package tests.wikipedia;
+
+import io.appium.java_client.AppiumBy;
+
+import org.junit.jupiter.api.Test;
+import tests.TestBase;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
+
+public class AndroidSearchTests extends TestBase {
+
+    @Test
+    void searchTest() {
+
+        step("Type search", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))
+                    .sendKeys("BrowserStack");
+        });
+        step("Verify content found", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+}
--- src/test/java/tests/wikipedia/OpenArticleInWikipediaTests.java
+++ src/test/java/tests/wikipedia/OpenArticleInWikipediaTests.java
@@ -1,27 +1,29 @@
-package tests;
+package tests.wikipedia;
 
 import io.appium.java_client.AppiumBy;
-
 import org.junit.jupiter.api.Test;
+import tests.TestBase;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
-public class AndroidSearchTests extends TestBase {
+public class OpenArticleInWikipediaTests extends TestBase {
 
     @Test
-    void searchTest() {
+    void openJavaArticle() {
 
         step("Type search", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();
             $(AppiumBy.accessibilityId("Search Wikipedia")).click();
             $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))
-                    .sendKeys("BrowserStack");
+                    .sendKeys("Java");
+            $(AppiumBy.accessibilityId("Java (programming language)")).click();
         });
+
         step("Verify content found", () ->
-                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(sizeGreaterThan(0)));
+                $(AppiumBy.accessibilityId("Java (programming language)"))
+                        .shouldHave(text("Java (programming language)")));
     }
+
 }
